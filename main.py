220bb98428cc9c79ac85121b7868077363aa62f5
Azamat <azamat_abdybekov@epam.com>
2024-05-06 22:10:08 +0600

    Readme file added. Payload and loop enabled in the code

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 
+locations = [
+             "london",
+             "шереметьево",
+             "череповец"
+]
 
-url_london = "https://wttr.in/london?nTqumM&lang=ru"
-url_sheremetevo = "https://wttr.in/%D1%88%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D1%82%D1%8C%D0%B5%D0%B2%D0%BE?nTqumM&lang=ru"
-url_cherepovec = "https://wttr.in/%D1%87%D0%B5%D1%80%D0%B5%D0%BF%D0%BE%D0%B2%D0%B5%D1%86?nTqumM&lang=ru"
+payload = {"nTqum": "", "lang": "ru"}
+base_url = "https://wttr.in/"
+for location in locations:
+    url = base_url + location
 
-
-
-for url in [url_london, url_sheremetevo, url_cherepovec]:
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     print(response.text)
