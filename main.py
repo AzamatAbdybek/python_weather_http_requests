f9a7d769a8426aa2d73aefb7ce7f4275582de4c1
Azamat Abdybekov <azamat_abdybekov@epam.com>
2024-08-09 20:31:30 +0600

    f-string and structure updated

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
 
+base_url = "https://wttr.in/"
 locations = [
              "london",
              "шереметьево",
              "череповец"
 ]
-
 payload = {"nTqum": "", "lang": "ru"}
-base_url = "https://wttr.in/"
-for location in locations:
-    url = base_url + location
 
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    print(response.text)
+def main():
+    for location in locations:
+        url = f"{base_url}{location}"
+
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
